6c44b164e72e5368a2d4314795d1c3ccf36d8dfa
Daniil-Vankevich <danya.vankevich@mail.ru>
2023-10-09 20:58:44 +0300

    переопределение переменных и создание клонов объектов

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> <?php echo "New page"; ?> </title>
+</head>
+
+<body>
+
+	<h1><?php
+			echo "Current datatime"; //current data input in page
+			?>
+	</h1>
+
+	<?php
+	$name = 'GeekBrains user';
+	echo "hello, $name!";
+	?>
+	<p> <?php
+			define('MY_NAME', 'Daniil');
+			echo MY_NAME;
+			?>
+	</p>
+
+	<div>
+		<?php
+		define('MY_CONST', 100);
+		echo MY_CONST;
+		?>
+	</div>
+
+	<?php
+	$a = 69;
+	echo "$a";
+	?>
+	<br>
+	<?
+	$a = (bool) $a;
+	echo '$a';
+	?>
+	<br>
+	<?
+	$a = (int) $a;
+	print($a);
+	?>
+	<br>
+
+	<?php
+
+	// include 'newincludes.php';
+
+	?>
+
+	<!-- ............................................ -->
+
+	<?php
+
+	$login = 'dan2002';
+	echo "Привет, '$login'";
+	?>
+	<br>
+	<?
+	echo "Салями, \"$login\"..........";
+
+	echo "Салями, \$login";
+
+	?>
+
+	<!-- .............function isset............................... -->
+	<br>
+	<?php
+	$user_login = 'strongAndHigh';
+	if (isset($user_login))
+		echo "Variable is true, $user_login";
+	else
+		echo "Variable is not true (it's false)";
+	?>
+
+	<br>
+	<!-- .............function empty............................... -->
+	<?php
+	$user_pass = "1223456";
+	if (empty($user_pass))
+		echo "Sring is empty";
+	else
+		echo "This string is not empty";
+	?>
+
+	<!-- .............function gettype............................... -->
+	<?php
+	//$gettype = gettype(123456789.5);
+	echo $gettype;
+	?>
+
+	<!-- .............условное ветвление вычитание из вещественного числа целого..................... -->
+	<br>
+
+	<?php
+	$num_test = 71;
+
+	$check = (float) ($num_test / 2) - (int) ($num_test / 2);
+
+	if ($check) {
+		echo "Number is odd";
+	} else {
+		echo "Numver is even";
+	}
+	?>
+
+	<br>
+	<!-- .............округление............................... -->
+
+	<?php
+	$frger = floor(69.569869752782);
+	echo $frger;
+	?>
+
+
+
+	<!-- Объекты и классы.................... -->
+
+	<?php 
+	
+	include 'newincludes.php';
+
+	$rabbit1 = new Rabbit;
+	$rabbit1->x = 13;
+	$rabbit1->y = 9;
+	echo $rabbit1->x;
+	?>
+	<br>
+	<?php
+	echo $rabbit1->y;
+
+	unset ($rabbit1);
+	echo $rabbit1->y;
+	echo $rabbit1->x;
+	?>
+	<br>
+
+	<?php 
+	
+	echo Rabbit:: $num;
+
+	?>
+
+	<br>
+
+		<!-- Переопределение переменных и ссылка объектов на одну и ту же область памяти -->
+
+		<?php 
+		
+		$first = $second = 2;
+		$first = 5;
+		echo $first;
+		echo $second;
+
+		?>
+
+		<br>
+		<br>
+
+		<?php 
+		$rabbit3 = new Rabbit;
+		$rabbit3->x = 69;
+		$rabbit3->y = 69;
+		
+		$rabbit4 = $rabbit3;
+		$rabbit4->x = 77;
+		$rabbit4->y = 77;
+		echo $rabbit3->x . '<br>' . $rabbit3->y ;
+		?>
+		
+		<br>
+		<br>
+		
+		<?php 
+		echo "x: {$rabbit3->x}, y: {$rabbit3->y}"
+		?>
+
+		<!-- Переменные также можно делать ссылками, если использовать знак амперсанта -->
+
+		<br>
+
+		<?php 
+		$daniil = 21;
+		$newnameismylife = &$daniil;
+		$newnameismylife = 22;
+		echo $daniil;
+		?>
+
+		<!-- клонирование объектов используя ключевое слово "clone" -->
+
+		<br>
+		<br>
+
+		<?php 
+			$rabbit5 = new Rabbit;
+			$rabbit5->z = 70;
+			$rabbit5->w = 70;
+
+			$rabbit6 = clone $rabbit5;
+			$rabbit6-> z = 99;
+			$rabbit6-> w = 99;
+
+			echo " z: {$rabbit6-> z}, w: {$rabbit6-> w}";
+			?>
+			<br>
+			
+			<?
+			echo " z: {$rabbit5-> z}, w: {$rabbit5-> w}";
+		?>
+		
+
+</body>
+
+</html>
---
+++ newincludes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+
+<body>
+	<h1><?php echo "Hello people!"; ?></h1>
+	<p><?= 'Perfect day is today)))))'; ?></p>
+
+	<?php 
+	class Rabbit {
+		public $x;
+		public $y;
+		public $z;
+		public $w;
+		public static $num =120;
+	}
+	?>
+
+</body>
+
+</html>
